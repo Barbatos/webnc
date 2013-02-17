3195f9bfa83de3b16d16b224638c7b3a67ff38de
HoLbLiN <holblin@holblin.com>
2013-02-17 16:33:03 +0100

    add socket.io and success connect chrome & node.js !

--- irc_0.3.js
+++ irc_0.3.js
@@ -1,4 +1,11 @@
+var io = require('socket.io').listen(8080);
 
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
 
 var channels_nokick = ['#csli']
 var irc = require('irc');
