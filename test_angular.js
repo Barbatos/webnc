662e43429c2cde5622e4483b27762d243d28dcf4
HoLbLiN <holblin@holblin.com>
2013-02-25 23:31:10 +0100

    better angular structure + add functions

--- test_angular.js
+++ test_angular.js
@@ -1,49 +1,137 @@
-function Webnc($scope) {
-  $scope.tabs = [
-    {name:'Quakenet', notifs: 8, channels: [
-        {name:'#frozensand', notifs: 0 },
-        {name:'pandy', notifs: 5 },
-        {name:'#mqcd', notifs: 0 },
-    ]},
-    {name:'Freenode', notifs: 5, channels:[
-        {name:'#csli', notifs: 3 },
-        {name:'_Tyr', notifs: 0 },
-        {name:'#test', notifs: 1 },
-    ]},
-  ];
-
-  $scope.tabs_messages = [
-    {
+var quakenet = {
       name: 'Quakenet',
       subject: 'Super sujet de la mort qui tue',
+      notifs : 5,
       messages : [
-        {date: '13:31', from: 'hoblin', content: 'Lorem ipsum dolor sit amet, consectetupteur sint <h1>occaecat</h1> cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-        {date: '13:33', from: 'pandy',  content: 'cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-        {date: '13:37', from: 'hoblin', content: 'Lorem ipsum dolor sit amet, consectetupteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-        {date: '13:37', from: 'pandy',  content: 'officia deserunt mollit anim id est laborum.' },
-        {date: '13:37', from: 'pandy',  content: 'Lorem ipsum dolor sit amet, consectetupteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-        {date: '13:38', from: 'hoblin', content: 'Lorem ipsum dolor sit amet, consectetupteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-        {date: '13:39', from: 'hoblin', content: 'officia deserunt mollit anim id est laborum.' }
+        {date: '13:31', from: 'holblin', content: 'Lorem ipsum dolor sit amet, consectetupteur sint <h1>occaecat</h1> cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:33', from: 'pandy',   content: 'cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:37', from: 'holblin', content: 'Lorem ipsum dolor sit amet, consectetupteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:37', from: 'pandy',   content: 'officia deserunt mollit anim id est laborum.' },
+        {date: '13:37', from: 'pandy',   content: 'Lorem ipsum dolor sit amet, consectetupteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:38', from: 'holblin', content: 'Lorem ipsum dolor sit amet, consectetupteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:39', from: 'holblin', content: 'officia deserunt mollit anim id est laborum.' }
       ]
-    },
-    {
+    };
+var frozensand = {
       name: '#frozensand',
-      subject: 'frozensand subject !!!',
+      subject: 'Super sujet de la mort qui tue',
+      notifs : 9,
       messages : [
-        {date: '13:31', from: 'hoblin', content: 'Lorem ipsum dolor sit amet, consectetupteur sint <h1>occaecat</h1> cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-        {date: '13:33', from: 'raider', content: 'cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-        {date: '13:37', from: 'hoblin', content: 'Lorem ipsum dolor sit amet, consectetupteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-        {date: '13:37', from: 'raider', content: 'officia deserunt mollit anim id est laborum.' },
-        {date: '13:37', from: 'raider', content: 'Lorem ipsum dolor sit amet, consectetupteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-        {date: '13:38', from: 'hoblin', content: 'Lorem ipsum dolor sit amet, consectetupteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-        {date: '13:39', from: 'hoblin', content: 'officia deserunt mollit anim id est laborum.' }
+        {date: '13:31', from: 'holblin', content: 'Lorem ipsum dolor sit amet, consectetupteur sint <h1>occaecat</h1> cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:33', from: 'pandy',   content: 'cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:37', from: 'holblin', content: 'Lorem ipsum dolor sit amet, consectetupteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:37', from: 'pandy',   content: 'officia deserunt mollit anim id est laborum.' },
+        {date: '13:37', from: 'pandy',   content: 'Lorem ipsum dolor sit amet, consectetupteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:38', from: 'holblin', content: 'Lorem ipsum dolor sit amet, consectetupteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:39', from: 'holblin', content: 'officia deserunt mollit anim id est laborum.' }
       ]
-    }
-  ];
+    };
+var pandy = {
+      name: 'pandy',
+      subject: 'Super sujet de la mort qui tue',
+      notifs : 0,
+      messages : [
+        {date: '13:31', from: 'holblin', content: 'Lorem ipsum dolor sit amet, consectetupteur sint <h1>occaecat</h1> cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:33', from: 'pandy',   content: 'cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:37', from: 'holblin', content: 'Lorem ipsum dolor sit amet, consectetupteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:37', from: 'pandy',   content: 'officia deserunt mollit anim id est laborum.' },
+        {date: '13:37', from: 'pandy',   content: 'Lorem ipsum dolor sit amet, consectetupteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:38', from: 'holblin', content: 'Lorem ipsum dolor sit amet, consectetupteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:39', from: 'holblin', content: 'officia deserunt mollit anim id est laborum.' }
+      ]
+    };
+var csli = {
+      name: 'csli',
+      subject: 'Super cslicsli csli la mort qui tue',
+      notifs : 0,
+      messages : [
+        {date: '13:31', from: 'holblin', content: 'Lorem ipsum csli sit amet, consectetupteur sint <h1>csli</h1> cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:33', from: 'pandy',   content: 'cupidatat non csli, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:37', from: 'holblin', content: 'Lorem ipsum dolor sit amet, csli sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:37', from: 'pandy',   content: 'officia deserunt mollit anim id est laborum.' },
+        {date: '13:37', from: 'pandy',   content: 'Lorem ipsum dolor sit amet, consectetupteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:38', from: 'holblin', content: 'Lorem ipsum dolor sit amet, consectetupteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:39', from: 'holblin', content: 'officia deserunt mollit anim id est laborum.' }
+      ]
+    };
+var freenode = {
+      name: 'Freenode',
+      subject: 'frozensand subject !!!',
+      notifs : 8,
+      messages : [
+        {date: '13:31', from: 'holblin', content: 'Lorem ipsum dolor sit amet, consectetupteur sint <h1>occaecat</h1> cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:33', from: 'raider',  content: 'cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:37', from: 'holblin', content: 'Lorem ipsum dolor sit amet, consectetupteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:37', from: 'raider',  content: 'officia deserunt mollit anim id est laborum.' },
+        {date: '13:37', from: 'raider',  content: 'Lorem ipsum dolor sit amet, consectetupteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:38', from: 'holblin', content: 'Lorem ipsum dolor sit amet, consectetupteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+        {date: '13:39', from: 'holblin', content: 'officia deserunt mollit anim id est laborum.' }
+      ]
+    };
 
+
+function Webnc($scope) {
+
+  $scope.tabs = [];
   $scope.selected_tab = 'Quakenet';
 
   $scope.selectTab = function( name ){
     $scope.selected_tab = name;
   }
+
+  $scope.test = function( arg ){
+    var i = 0;
+    for (i = 0 ; i < 10 ; i++){
+      $scope.addMessage( 0 , null, {date: '13:96', from:'asche', content:'TOTO'} );
+      $scope.addMessage( 0 , 0, {date: '13:96', from:'asche', content:'TOTO'} );
+    }
+  }
+
+  $scope.addServer = function( serverKey , server ){
+    // We REFUSE to REPLACE a server
+    if ( typeof $scope.tabs[serverKey] != "undefined" )
+      return false;
+
+    $scope.tabs[serverKey] = server;
+    $scope.tabs[serverKey].channels = [];
+    return true;
+  }
+
+  $scope.addChannel = function( serverKey , channelKey,  channel ){
+    // serverKey MUST be set
+    if ( typeof $scope.tabs[serverKey] == "undefined" )
+      return false;
+
+    // We REFUSE to REPLACE a channel
+    if ( typeof $scope.tabs[serverKey].channels[ channelKey ] != "undefined" )
+      return false;
+
+    $scope.tabs[serverKey].channels[ channelKey ] = channel;
+    return true;
+  }
+
+  $scope.addMessage = function( serverKey, channelKey, message ){
+    // serverKey MUST be set
+    if ( typeof $scope.tabs[serverKey] == "undefined" )
+      return false;
+    
+    if (channelKey == null ){
+      $scope.tabs[serverKey].messages.push( message );
+      return true;
+    }
+    else {
+      // channelKey must be set too
+      if ( typeof $scope.tabs[serverKey].channels[ channelKey ] == "undefined" )
+        return false;
+
+      $scope.tabs[serverKey].channels[ channelKey ].messages.push( message );
+      return true;
+    }
+  }
+
+  $scope.addServer( 0 , quakenet );
+  $scope.addChannel( 0 , 0, frozensand );
+  $scope.addChannel( 0 , 1, pandy );
+  $scope.addServer( 1 , freenode );
+  $scope.addChannel( 1 , 0, csli );
 }
